09a3d5abec35ee2fdda0561e071d2b947325cc01
bakaoh <tatattai@gmail.com>
2018-07-26 09:16:35 +0700

    Fix dateformat bug

--- app/src/main/java/com/bakaoh/bigchaindb/MainActivity.java
+++ app/src/main/java/com/bakaoh/bigchaindb/MainActivity.java
@@ -1,17 +1,22 @@
 package com.bakaoh.bigchaindb;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.authenteq.api.AssetsApi;
+import com.authenteq.builders.BigchainDbConfigBuilder;
 import com.authenteq.builders.BigchainDbTransactionBuilder;
 import com.authenteq.constants.Operations;
 import com.authenteq.json.strategy.MetaDataDeserializer;
 import com.authenteq.json.strategy.MetaDataSerializer;
 import com.authenteq.model.Account;
+import com.authenteq.model.Assets;
 import com.authenteq.model.MetaData;
 import com.authenteq.model.MetaDatas;
 import com.authenteq.model.Transaction;
+import com.authenteq.util.JsonUtils;
 
 import net.i2p.crypto.eddsa.EdDSAPrivateKey;
 import net.i2p.crypto.eddsa.EdDSAPublicKey;
@@ -28,13 +33,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+
         Map<String, String> assetData = new TreeMap<String, String>() {{
-            put("msg", "Hello BigchainDB!");
+            put("msg", "Test");
         }};
+
+        // JsonUtils.setJsonDateFormat("yyyy-MM-dd'T'HH:mm:ssZZZZZ");
+
+        BigchainDbConfigBuilder
+                .baseUrl("https://test.bigchaindb.com")
+                .addToken("app_id", "")
+                .addToken("app_key", "").setup();
 
         MetaData metaData = new MetaData();
         metaData.setId("51ce82a14ca274d43e4992bbce41f6fdeb755f846e48e710a3bbb3b0cf8e4204");
-        metaData.setMetaData("msg", "Metadata1");
+        metaData.setMetaData("msg", "Testing Android");
 
 
         setContentView(R.layout.activity_main);
@@ -48,12 +66,25 @@ public class MainActivity extends AppCompatActivity {
                     .addMetaDataClassSerializer(MetaData.class, new MetaDataSerializer())
                     .addMetaDataClassDeserializer(MetaDatas.class, new MetaDataDeserializer())
                     .operation(Operations.CREATE)
-                    .buildAndSignOnly(
+                    .buildAndSign(
                             (EdDSAPublicKey) Account.publicKeyFromHex(publicKey),
-                            (EdDSAPrivateKey) Account.privateKeyFromHex(privateKey));
+                            (EdDSAPrivateKey) Account.privateKeyFromHex(privateKey))
+                    .sendTransaction();
+
+            Assets assets = AssetsApi.getAssets("Test");
+
+
             TextView text = findViewById(R.id.hello);
-            text.setText(transaction.toString());
-        } catch (InvalidKeySpecException e) {
+            String data = "";
+            if(assets.getAssets().size() > 0){
+                data = assets.getAssets().get(0).getData().toString();
+            }
+            else {
+                data = "No Assets found";
+            }
+
+            text.setText(data);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
